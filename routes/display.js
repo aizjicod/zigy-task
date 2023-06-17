8fa4f7d29061e052b546d5d04d1729bff7713c16
Aiziji <aletv1501@gmail.com>
2023-06-18 00:38:08 +0800

    Add imputs on index

--- routes/display.js
+++ routes/display.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../module/data')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('display', { data: `${data.items === undefined ? [] : data.items}` });
+  // localStorage.getItem('Data', JSON.stringify(data));
+  res.render('display', { data: [`data`] });
 });
 
 module.exports = router;
