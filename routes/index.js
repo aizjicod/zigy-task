a24c93e225412ca846b038234f8662aaa2d564ba
Aiziji <aletv1501@gmail.com>
2023-06-18 18:05:24 +0800

    Implement readsheets from google

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config()
+const {
+  getAuthToken,
+  getSpreadSheetValues
+} = require('../googleSheetsService');
+
+const spreadsheetId = process.env.SHEETS_ID;
+const sheetName = process.env.SHEETS_NAME;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+async function testGetSpreadSheetValues() {
+}
+
+router.get('/', async function(req, res, next) {
+  try {
+    const auth = await getAuthToken();
+    const response = await getSpreadSheetValues({
+      spreadsheetId,
+      sheetName,
+      auth
+    })
+    console.log('output for getSpreadSheetValues', JSON.stringify(response.data, null, 2));
+    res.render('index', { title: 'Express' });
+  } catch(error) {
+    res.render('index', { message: error.message, error: error.stack});
+  }
 });
 
 module.exports = router;
